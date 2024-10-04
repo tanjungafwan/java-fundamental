dfb14ac49e81966e366aa6bedf1746d2487cee23
Afwan <rizki.afwan99@gmail.com>
2024-10-04 22:57:27 +0700

    First Commit

---
+++ src/main/java/com/juaracoding/FirstJava.java
@@ -0,0 +1,20 @@
+package com.juaracoding;
+
+public class FirstJava {
+    public static void main(String[] args) {
+        System.out.println("Hello JuaraCoding!");
+        System.out.println("Indonesia");
+        int x = 100;
+
+        if(true){
+            System.out.println("true");
+            System.out.println(x);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+            System.out.println(x);
+        }
+
+    }
+}
